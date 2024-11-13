543ff0bcd2ba8c43b7951c9d796fe21693f262cf
hkoos <670999782@qq.com>
2024-11-13 22:37:45 +0800

    修复resize时内容丢失

--- lib/src/utils/circular_buffer.dart
+++ lib/src/utils/circular_buffer.dart
@@ -244,12 +244,12 @@ class IndexAwareCircularBuffer<T extends IndexedItem> {
       _dropChild(i);
     }
 
+    _startIndex = 0;
     final copyLength = replacement.length - copyStart;
     for (var i = 0; i < copyLength; i++) {
       _adoptChild(i, replacement[copyStart + i]);
     }
 
-    _startIndex = 0;
     _length = copyLength;
   }
 
